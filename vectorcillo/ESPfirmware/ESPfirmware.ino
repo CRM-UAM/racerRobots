19073eda9b68742cc70edd17c1934685b70725a3
Carlos Garcia Saura <carlosgarciasaura@gmail.com>
2016-04-30 19:50:05 +0200

    Primeras pruebas de movimiento

--- vectorcillo/ESPfirmware/ESPfirmware.ino
+++ vectorcillo/ESPfirmware/ESPfirmware.ino
@@ -19,8 +19,40 @@ void handleRoot() {
 	server.send(200, "text/html", "<h1>You are connected</h1>");
 }
 
+#define PIN_SPEED_A 12
+#define PIN_SPEED_B 13
+
+#define PIN_DIR_A 15
+#define PIN_DIR_B 2
+
+#define PIN_SENSOR_R 14
+#define PIN_SENSOR_L 16
+
+#define PIN_BUTTON 0
+
 void setup() {
+  pinMode(PIN_BUTTON, INPUT);
+  
+  pinMode(PIN_SENSOR_L, INPUT_PULLUP);
+  pinMode(PIN_SENSOR_R, INPUT_PULLUP);
+  
+  pinMode(PIN_SPEED_A, OUTPUT);
+  pinMode(PIN_SPEED_B, OUTPUT);
+
+  pinMode(PIN_DIR_A, OUTPUT);
+  pinMode(PIN_DIR_B, OUTPUT);
+
+
+  analogWrite(PIN_SPEED_A, 0);
+  analogWrite(PIN_SPEED_B, 0);
+  
+  analogWrite(PIN_DIR_A, 0);
+  analogWrite(PIN_DIR_B, 0);
+
+  
 	delay(1000);
+
+  
 	Serial.begin(115200);
 	Serial.println();
 	Serial.print("Configuring access point...");
@@ -37,4 +69,8 @@ void setup() {
 
 void loop() {
 	server.handleClient();
+  if(!digitalRead(PIN_BUTTON)) {
+    analogWrite(PIN_SPEED_B, 220);
+    delay(3000);
+  } else analogWrite(PIN_SPEED_B, 0);
 }
