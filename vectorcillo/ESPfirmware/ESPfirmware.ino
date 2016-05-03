691ea69da622c9c733fbf71ab24afe3dcfd0bcfc
Carlos Garcia Saura <carlosgarciasaura@gmail.com>
2016-05-03 13:27:50 +0200

    Testing the direction

--- vectorcillo/ESPfirmware/ESPfirmware.ino
+++ vectorcillo/ESPfirmware/ESPfirmware.ino
@@ -1,23 +1,22 @@
 
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h> 
+#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <ESP8266HTTPUpdateServer.h>
 #include <WiFiUdp.h>
-#include <ArduinoOTA.h>
 
 #include "credentials.h"
 //const char ssid[] = "XXXX"; // network SSID (name)
 //const char pass[] = "XXXX"; // network password
 
-ESP8266WebServer server(80);
+ESP8266WebServer httpServer(80);
+ESP8266HTTPUpdateServer httpUpdater;
+WiFiServer telnet_server(23);
+WiFiClient telnet;
 
-/* Just a little test message.  Go to http://192.168.4.1 in a web browser
- * connected to this access point to see it.
- */
-void handleRoot() {
-	server.send(200, "text/html", "<h1>You are connected</h1>");
-}
+// Go to http://192.168.4.1 in a web browser
+// connected to this access point to update it
 
 #define PIN_SPEED_A 12
 #define PIN_SPEED_B 13
@@ -30,79 +29,149 @@ void handleRoot() {
 
 #define PIN_BUTTON 0
 
+bool buttonIsPressed() {
+  return !digitalRead(PIN_BUTTON);
+}
+
+void turnOffMotors() {
+  analogWrite(PIN_SPEED_A, 0);
+  analogWrite(PIN_SPEED_B, 0);
+
+  analogWrite(PIN_DIR_A, 0);
+  analogWrite(PIN_DIR_B, 0);
+}
+
+
+void steering(int dir) {
+  if (dir > 1000) dir = 1000;
+  if (dir < -1000) dir = -1000;
+  if (dir > 0) {
+    analogWrite(PIN_DIR_A, dir);
+    analogWrite(PIN_DIR_B, 0);
+  } else {
+    analogWrite(PIN_DIR_A, 0);
+    analogWrite(PIN_DIR_B, -dir);
+  }
+  delay(1); // Yield some CPU cycles to ESP8266 (to avoid watchdog restarts)
+}
+
+void setDir(int dir) {
+  /*if (abs(dir) < 50) {
+    while (!digitalRead(PIN_SENSOR_R) || !digitalRead(PIN_SENSOR_L)) {
+      while (!digitalRead(PIN_SENSOR_R)) steering(500);
+      while (!digitalRead(PIN_SENSOR_L)) steering(-500);
+    }
+  } else */if (dir > 0) {
+    while (!digitalRead(PIN_SENSOR_R) || digitalRead(PIN_SENSOR_L)) steering(500);
+  } else if (dir < 0) {
+    while (digitalRead(PIN_SENSOR_R) || !digitalRead(PIN_SENSOR_L)) steering(-500);
+  }
+  steering(dir);
+}
+
+
 void setup() {
   pinMode(PIN_BUTTON, INPUT);
-  
+
   pinMode(PIN_SENSOR_L, INPUT_PULLUP);
   pinMode(PIN_SENSOR_R, INPUT_PULLUP);
-  
+
   pinMode(PIN_SPEED_A, OUTPUT);
   pinMode(PIN_SPEED_B, OUTPUT);
 
   pinMode(PIN_DIR_A, OUTPUT);
   pinMode(PIN_DIR_B, OUTPUT);
 
+  analogWriteFreq(20000); //
 
-  analogWrite(PIN_SPEED_A, 0);
-  analogWrite(PIN_SPEED_B, 0);
-  
-  analogWrite(PIN_DIR_A, 0);
-  analogWrite(PIN_DIR_B, 0);
+  turnOffMotors();
 
-  
-	delay(1000);
 
-  
-	Serial.begin(115200);
-	Serial.println();
-	Serial.print("Configuring access point...");
-	/* You can remove the password parameter if you want the AP to be open. */
-	WiFi.softAP(ssid, pass);
+  delay(1000);
 
-	IPAddress myIP = WiFi.softAPIP();
-	Serial.print("AP IP address: ");
-	Serial.println(myIP);
-	server.on("/", handleRoot);
-	server.begin();
-	Serial.println("HTTP server started");
+  Serial.begin(250000);
 
-  ArduinoOTA.onStart([]() {
-    Serial.println("Start");
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
-  });
-  ArduinoOTA.begin();
+  Serial.print("\nConfiguring access point...");
+  WiFi.softAP(ssid, pass);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  httpUpdater.setup(&httpServer);
+  httpServer.begin();
+  Serial.println("HTTP server started");
+
+  delay(1000);
+
+  telnet_server.begin();
+  //telnet_server.setNoDelay(true);
+  Serial.println("Telnet server started");
+
   Serial.println("Ready");
 }
 
+
+
+
+
+float gyro, maxAcc, maxAccLateral, line, lineDt, minDistFront, minDistL, minDistR, distDtFront, distDtL, distDtR;
+
 void loop() {
-  ArduinoOTA.handle();
-	server.handleClient();
-  if(!digitalRead(PIN_BUTTON)) {
-    analogWrite(PIN_DIR_B, 300);
-    delay(1000);
-    analogWrite(PIN_DIR_B, 0);
-    analogWrite(PIN_DIR_A, 300);
-    delay(1000);
-    analogWrite(PIN_SPEED_B, 500);
-    delay(3000);
-  } else {
+  if (telnet_server.hasClient()) {
+    if (telnet) telnet.stop();
+    telnet = telnet_server.available();
+  }
+  if (Serial.available()) { // && telnet && telnet.connected()) {
+    /*float val = Serial.parseFloat();
+      setDir(val);
+      delay(1000);
+      while (Serial.available()) Serial.read();*/
+
+    while (Serial.read() != '\n');
+
+    gyro = Serial.parseFloat();
+    maxAcc = Serial.parseFloat();
+    maxAccLateral = Serial.parseFloat();
+    line = Serial.parseFloat();
+    lineDt = Serial.parseFloat();
+    minDistFront = Serial.parseFloat();
+    minDistL = Serial.parseFloat();
+    minDistR = Serial.parseFloat();
+    distDtFront = Serial.parseFloat();
+    distDtL = Serial.parseFloat();
+    distDtR = Serial.parseFloat();
+
+    /*if(telnet && telnet.connected()) {
+      telnet.print(gyro); telnet.print(" ");
+      telnet.print(maxAcc); telnet.print(" ");
+      telnet.print(maxAccLateral); telnet.print(" ");
+      telnet.print(line); telnet.print(" ");
+      telnet.print(lineDt); telnet.print(" ");
+      telnet.print(minDistFront); telnet.print(" ");
+      telnet.print(minDistL); telnet.print(" ");
+      telnet.print(minDistR); telnet.print(" ");
+      telnet.print(distDtFront); telnet.print(" ");
+      telnet.print(distDtL); telnet.print(" ");
+      telnet.print(distDtR); telnet.println();
+      }*/
+    float lineError = line - 3500;
+    if (telnet && telnet.connected()) telnet.println(line);
+    setDir(map(line, 7000, 0, -600, 600));
+    //steering(map(line, 7000, 0, -900, 900));
+  }
+
+  httpServer.handleClient();
+
+  if (buttonIsPressed()) {
+    analogWrite(PIN_SPEED_B, 0);
+    delay(200);
+    if (buttonIsPressed()) analogWrite(PIN_SPEED_B, 500);
+    delay(500);
+  } /*else {
     analogWrite(PIN_SPEED_A, 0);
     analogWrite(PIN_SPEED_B, 0);
     analogWrite(PIN_DIR_A, 0);
     analogWrite(PIN_DIR_B, 0);
-    
-  }
+
+  }*/
 }
--- vectorcillo/arduinoFirmware/arduinoFirmware.ino
+++ vectorcillo/arduinoFirmware/arduinoFirmware.ino
@@ -28,24 +28,26 @@
 #define DIST_3_PIN      A2
 #define DIST_2_PIN      A1
 #define DIST_1_PIN      A0
-unsigned int defAnalogPIN[5]={DIST_1_PIN,DIST_2_PIN,DIST_3_PIN,DIST_4_PIN,DIST_5_PIN};
+unsigned int defAnalogPIN[5] = {DIST_1_PIN, DIST_2_PIN, DIST_3_PIN, DIST_4_PIN, DIST_5_PIN};
 
 
 
-unsigned int line_pos_pre=0;
-unsigned long last_time_line=0, last_time_IMU=0, last_time_distance=0;
-float line_derivate=0;
-unsigned int line_pos=0;
+unsigned int line_pos_pre = 0;
+unsigned long last_time_line = 0, last_time_IMU = 0, last_time_distance = 0, last_time_sent = 0;
+float line_derivate = 0;
+unsigned int line_pos = 0;
 unsigned int line_values[NUM_IR_SENSORS];
 float dist_values[NUM_DIST_SENSORS];
-float dist_min_values[NUM_DIST_SENSORS]={1000};
-float dist_last_values[NUM_DIST_SENSORS]={0};
-float dist_derivate_values[NUM_DIST_SENSORS]={0};
+float dist_min_values[NUM_DIST_SENSORS] = {1000};
+float dist_last_values[NUM_DIST_SENSORS] = {0};
+float dist_derivate_values[NUM_DIST_SENSORS] = {0};
 
-QTRSensorsRC qtrrc((unsigned char[]) {IR1, IR2, IR3, IR4, IR5, IR6, IR7, IR8}, NUM_IR_SENSORS, TIMEOUT, QTR_NO_EMITTER_PIN); // IR emitter is always ON
+QTRSensorsRC qtrrc((unsigned char[]) {
+  IR1, IR2, IR3, IR4, IR5, IR6, IR7, IR8
+}, NUM_IR_SENSORS, TIMEOUT, QTR_NO_EMITTER_PIN); // IR emitter is always ON
 
 
-const int MPU=0x68;  // I2C address of the MPU-6050
+const int MPU = 0x68; // I2C address of the MPU-6050
 
 #define MPU6050_PWR_MGMT_1    0x6B
 #define MPU6050_ACCEL_CONFIG  0x1C
@@ -53,8 +55,8 @@ const int MPU=0x68;  // I2C address of the MPU-6050
 #define MPU6050_SMPLRT_DIV    0x19
 #define MPU6050_CONFIG        0x1A
 
-int16_t AcX=0, AcY=0, GyZ=0;
-float max_accel=0, max_accel_lateral=0;
+int16_t AcX = 0, AcY = 0, GyZ = 0;
+float max_accel = 0, max_accel_lateral = 0;
 int16_t AcXoffset, AcYoffset, GyZoffset;
 float GyZ_integral;
 
@@ -73,14 +75,14 @@ bool readIMU(int16_t *AcX, int16_t *AcY, int16_t *GyZ) {
   Wire.beginTransmission(MPU);
   Wire.write(0x3B);  // starting with register 0x3D (ACCEL_YOUT_H)
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU,14,true);  // request a total of 14 registers
-  new_AcX = Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-  new_AcY = Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  for(int i=0; i<8; i++) Wire.read(); // Discard 0x3F-0x46
-  new_GyZ = Wire.read()<<8 | Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  Wire.requestFrom(MPU, 14, true); // request a total of 14 registers
+  new_AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  new_AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  for (int i = 0; i < 8; i++) Wire.read(); // Discard 0x3F-0x46
+  new_GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 
   updated = (*AcX != new_AcX) || (*AcY != new_AcY) || (*GyZ != new_GyZ); // Check if value has changed
-  
+
   *AcX = new_AcX;
   *AcY = new_AcY;
   *GyZ = new_GyZ;
@@ -89,7 +91,7 @@ bool readIMU(int16_t *AcX, int16_t *AcY, int16_t *GyZ) {
 
 void init_IMU() {
   Serial.println(F("Calibrating IMU..."));
-  for(int i=0; i<3; i++) { // Reset the IMU a few times
+  for (int i = 0; i < 3; i++) { // Reset the IMU a few times
     IMUwriteReg(MPU6050_PWR_MGMT_1, bit(7) );  // DEVICE_RESET to 1 (D7=1)
     delay(100);
   }
@@ -110,7 +112,7 @@ void init_IMU() {
   AcXoffset = 0;
   AcYoffset = 0;
   GyZoffset = 0;
-  for(int i=0; i<10; i++) {
+  for (int i = 0; i < 10; i++) {
     readIMU(&AcX, &AcY, &GyZ);
     AcXoffset += AcX;
     AcYoffset += AcY;
@@ -130,31 +132,31 @@ void init_IMU() {
 
 
 
-void calibrateIR(int time){
+void calibrateIR(int time) {
   int i;
   Serial.println(F("Calibrating line sensor... please sweep the sensor across a black line a few times"));
   //ledOn();
   qtrrc.resetCalibration();
-  unsigned long targetTime = millis()+5000;
-  while(millis() < targetTime) {
+  unsigned long targetTime = millis() + 5000;
+  while (millis() < targetTime) {
     /*qtrrc.read(line_values);
-    for(i=0; i<NUM_IR_SENSORS; i++){
+      for(i=0; i<NUM_IR_SENSORS; i++){
         Serial.print(line_values[i]);
         Serial.print(" ");
-    }
-    Serial.println();*/
+      }
+      Serial.println();*/
     qtrrc.calibrate(); // reads all sensors at 2500 us per read (i.e. ~25 ms per call)
   }
   //ledOff();
   //print results
   Serial.println(F("Results\nMinimum: "));
-  for(i=0; i<NUM_IR_SENSORS; i++){
+  for (i = 0; i < NUM_IR_SENSORS; i++) {
     Serial.print(qtrrc.calibratedMinimumOn[i]);
     Serial.print(F(" "));
   }
   Serial.println();
   Serial.print(F("Maximum: "));
-  for(i=0; i<NUM_IR_SENSORS; i++){
+  for (i = 0; i < NUM_IR_SENSORS; i++) {
     Serial.print(qtrrc.calibratedMaximumOn[i]);
     Serial.print(F(" "));
   }
@@ -163,18 +165,18 @@ void calibrateIR(int time){
 
 
 float analogReadAverage(int pin, int samples) {
-    float result = 0;
-    for(int i=0; i<samples; i++) {
-        result += analogRead(pin);
-        delayMicroseconds(100);
-    }
-    return result/(float)samples;
+  float result = 0;
+  for (int i = 0; i < samples; i++) {
+    result += analogRead(pin);
+    delayMicroseconds(100);
+  }
+  return result / (float)samples;
 }
 
 float getDistanceCM(int pin) {
-    float res = 4419.36/((float)analogReadAverage(pin,4)-32.736); // Convert ADC value to centimeters
-    if(res < 0 || res > 150) res = 150;
-    return res;
+  float res = 4419.36 / ((float)analogReadAverage(pin, 4) - 32.736); // Convert ADC value to centimeters
+  if (res < 0 || res > 150) res = 150;
+  return res;
 }
 
 
@@ -185,9 +187,9 @@ void setup() {
   Serial.println(F("Starting up sensor board..."));
 
   init_IMU();
-  
+
   calibrateIR(5);
-  
+
   GyZ_integral = 0;
 }
 
@@ -195,86 +197,93 @@ void setup() {
 
 
 void sendValues2ESP() {
-    int i;
-    //Serial.print(micros()/1000000.);
-    //Serial.print(",");
-    Serial.print(F("Gyr: "));
-    Serial.print((GyZ_integral/3000.)*180.);
-
-    Serial.print(F(" maxAccXY: "));
-    Serial.print(max_accel);
+  int i;
+  //Serial.print(micros()/1000000.);
+  //Serial.print(",");
+  //Serial.print(F("Gyr: "));
+  Serial.print((GyZ_integral / 3000.) * 180.);
+  Serial.print(F(" "));
+  
+  //Serial.print(F("maxAccXY: "));
+  Serial.print(max_accel);
+  Serial.print(F(" "));
+  Serial.print(max_accel_lateral);
+  Serial.print(F(" "));
+  
+  //Serial.print(F("Line: "));
+  Serial.print(line_pos);
+  Serial.print(F(" "));
+  
+  //Serial.print(F("LineDt: "));
+  Serial.print(line_derivate);
+  Serial.print(F(" "));
+  
+  //Serial.print(F("minDist: "));
+  for (i = 0; i < NUM_DIST_SENSORS; i++) {
+    Serial.print(dist_min_values[i]);
     Serial.print(F(" "));
-    Serial.print(max_accel_lateral);
-    
-    Serial.print(F(" Line: "));
-    Serial.print(line_pos);
-    
-    Serial.print(F(" LineDt: "));
-    Serial.print(line_derivate);
-    
-    Serial.print(F(" minDist: "));
-    for(i=0; i<NUM_DIST_SENSORS; i++){
-        Serial.print(dist_min_values[i]);
-        Serial.print(F(" "));
-    }
-    
-    Serial.print(F(" DistDt: "));
-    for(i=0; i<NUM_DIST_SENSORS; i++){
-        Serial.print(dist_derivate_values[i]);
-        Serial.print(F(" "));
-    }
-    /*for(i=0; i<NUM_IR_SENSORS; i++){
-        Serial.print(line_values[i]);
-        Serial.print(" ");
+  }
+
+  //Serial.print(F(" DistDt: "));
+  for (i = 0; i < NUM_DIST_SENSORS; i++) {
+    Serial.print(dist_derivate_values[i]);
+    Serial.print(F(" "));
+  }
+  /*for(i=0; i<NUM_IR_SENSORS; i++){
+      Serial.print(line_values[i]);
+      Serial.print(" ");
     }*/
-    Serial.println();
+  Serial.println();
 }
 
 
 void loop() {
   int i;
-  
+  bool sendValues = false;
   bool changed = readIMU(&AcX, &AcY, &GyZ);// check if value has been updated
   unsigned long current = micros();
-  float dt = ((float)(current-last_time_IMU))/1000000.;
-  if(changed) {
-    if(dt > 0) {
-      GyZ_integral += (GyZ-GyZoffset)*dt;
+  float dt = ((float)(current - last_time_IMU)) / 1000000.;
+  if (changed) {
+    if (dt > 0) {
+      GyZ_integral += (GyZ - GyZoffset) * dt;
       last_time_IMU = current;
     }
-    float accel = (AcX-AcXoffset)*COS45 + (AcY-AcYoffset)*COS45; // The sensor is rotated 45 degrees, we need to account for that
-    float accel_lateral = (AcX-AcXoffset)*COS45 - (AcY-AcYoffset)*COS45;
-    if(abs(accel) > abs(max_accel)) max_accel = accel;
-    if(abs(accel_lateral) > abs(max_accel_lateral)) max_accel_lateral = accel_lateral;
+    float accel = (AcX - AcXoffset) * COS45 + (AcY - AcYoffset) * COS45; // The sensor is rotated 45 degrees, we need to account for that
+    float accel_lateral = (AcX - AcXoffset) * COS45 - (AcY - AcYoffset) * COS45;
+    if (abs(accel) > abs(max_accel)) max_accel = accel;
+    if (abs(accel_lateral) > abs(max_accel_lateral)) max_accel_lateral = accel_lateral;
   }
 
   line_pos = qtrrc.readLine(line_values);
   current = micros();
-  dt = ((float)(current-last_time_line))/1000000.;
-  if(dt > 0.1) {
-    line_derivate = ((float)line_pos - (float)line_pos_pre)/dt;
+  dt = ((float)(current - last_time_line)) / 1000000.;
+  if (dt > 0.1) {
+    line_derivate = ((float)line_pos - (float)line_pos_pre) / dt;
     line_pos_pre = line_pos;
     last_time_line = current;
   }
 
-  for(i=0; i<NUM_DIST_SENSORS; i++) {
+  for (i = 0; i < NUM_DIST_SENSORS; i++) {
     dist_values[i] = getDistanceCM(defAnalogPIN[i]);
-    if(dist_values[i] < dist_min_values[i]) dist_min_values[i] = dist_values[i];
+    if (dist_values[i] < dist_min_values[i]) dist_min_values[i] = dist_values[i];
   }
   current = micros();
-  dt = ((float)(current-last_time_distance))/1000000.;
-  if(dt > 0.1) {
-    for(i=0; i<NUM_DIST_SENSORS; i++) dist_derivate_values[i] = (dist_values[i] - dist_last_values[i])/dt;
-    for(i=0; i<NUM_DIST_SENSORS; i++) dist_last_values[i] = dist_values[i];
+  dt = ((float)(current - last_time_distance)) / 1000000.;
+  if (dt > 0.1) {
+    for (i = 0; i < NUM_DIST_SENSORS; i++) dist_derivate_values[i] = (dist_values[i] - dist_last_values[i]) / dt;
+    for (i = 0; i < NUM_DIST_SENSORS; i++) dist_last_values[i] = dist_values[i];
     last_time_distance = current;
   }
 
-  if(Serial.available() > 0) {
+  current = micros();
+  dt = ((float)(current - last_time_sent)) / 1000000.;
+  if (dt > 0.05) {//Serial.available() > 0) { // RX pin is dead :-(
     sendValues2ESP();
-    while(Serial.available() > 0) Serial.read();
-    for(i=0; i<NUM_DIST_SENSORS; i++) dist_min_values[i]=1000;
+    //while (Serial.available() > 0) Serial.read();
+    for (i = 0; i < NUM_DIST_SENSORS; i++) dist_min_values[i] = 1000;
     max_accel = 0;
     max_accel_lateral = 0;
+    last_time_sent = current;
   }
 
 }
